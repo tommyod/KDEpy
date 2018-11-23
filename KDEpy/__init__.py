c494a5ca339fe855dfb3e06dd70c79b0490a9b9d
tommyod <tommy.odland@gmail.com>
2018-11-23 15:47:05 +0100

    incremented version to 0.6.1

--- KDEpy/__init__.py
+++ KDEpy/__init__.py
@@ -7,7 +7,7 @@ from KDEpy.TreeKDE import TreeKDE
 from KDEpy.FFTKDE import FFTKDE
 
 # If this is incremented, also increment in setup.py
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 __author__ = 'tommyod'
 
 TreeKDE = TreeKDE
