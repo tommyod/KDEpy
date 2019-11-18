8fb8dce51d17e9cf69382125ceb8b3e24c1c20a2
Tommy <10076072+tommyod@users.noreply.github.com>
2019-11-18 19:49:23 +0100

    [MRG] Deployment on master merge (#34)
    * Updated travis with deployment
    
    * deployment script
    
    * fixed version
    
    * changed travis
    
    * added deployment stuff back
    
    * updated deployment script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+$PIP install cibuildwheel;
+$PYTHON -m cibuildwheel --output-dir wheelhouse;
+$PIP install twine;
+$PYTHON -m twine upload dist/* -u tommyod -p $TWINE_PASSWORD --skip-existing;
+$PYTHON -m twine upload wheelhouse/* -u tommyod -p $TWINE_PASSWORD --skip-existing;
