608e457e8e97fef90bf2c4793328e741657608b9
tommyod <tommy.odland@gmail.com>
2019-11-18 19:42:13 +0100

    updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ $PIP install cibuildwheel;
 $PYTHON -m cibuildwheel --output-dir wheelhouse;
 $PIP install twine;
 $PYTHON -m twine upload dist/* -u tommyod -p $TWINE_PASSWORD --skip-existing;
+$PYTHON -m twine upload wheelhouse/* -u tommyod -p $TWINE_PASSWORD --skip-existing;
