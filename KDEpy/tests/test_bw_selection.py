b9df7c885b2066af261b70f95733108ac54c7275
Luke Dyer <luke.dyer@peak.ai>
2020-11-17 19:57:33 +0000

    adds test for bandwidth weight being the same as resampling

--- KDEpy/tests/test_bw_selection.py
+++ KDEpy/tests/test_bw_selection.py
@@ -34,6 +34,17 @@ def test_ISJ_bw_weights_single_zero_weighted_point(data):
     )
 
 
+# multiple runs to allow a good spread of catching errors
+@pytest.mark.parametrize("execution_number", range(5))
+def test_isj_bw_weights_same_as_resampling(data, execution_number):
+    sample_weights = np.random.randint(low=1, high=100, size=len(data))
+    data_resampled = np.repeat(data, repeats=sample_weights).reshape((-1, 1))
+    np.testing.assert_array_almost_equal(
+        improved_sheather_jones(data_resampled),
+        improved_sheather_jones(data, sample_weights),
+    )
+
+
 if __name__ == "__main__":
     # --durations=10  <- May be used to show potentially slow tests
     pytest.main(args=[__file__, "--doctest-modules", "-v", "--durations=15"])
