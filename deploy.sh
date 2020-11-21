1572c40221ba18c7dfc44daf3fdd790e9994d275
tommyod <tommy.odland@gmail.com>
2020-11-21 09:21:10 +0100

    deleted old bash deploy file

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-$PIP install cibuildwheel;
-$PYTHON -m cibuildwheel --output-dir wheelhouse;
-$PIP install twine;
-$PYTHON -m twine upload wheelhouse/* -u tommyod -p $TWINE_PASSWORD --skip-existing;
