d3ea7893082a4f3c05dc44ec7587b296626bca8c
tommyod <tommy.odland@gmail.com>
2023-05-07 14:42:16 +0200

    removed py37 support

--- setup.py
+++ setup.py
@@ -58,8 +58,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
