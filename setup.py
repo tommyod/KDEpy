937eb800b4d5d932fc1ed68aa5b502e3b4409602
Scott White <whitews@gmail.com>
2023-09-21 10:18:57 -0400

    Fix cutils location (#152)
    * compile cutils inside package dir (see #150)
    - cutils.pyx moved to its own dir to avoid namespace collision
    * ignore Cython generated cutils.c
    * removed unused setup code (from before pyproject.toml change)
    * extension path is case-sensitive
    * fix formatting & bump version

--- setup.py
+++ setup.py
@@ -7,22 +7,13 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-import os
-
 import numpy as np
 from Cython.Distutils import build_ext
 from setuptools import Extension, setup
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(fname):
-    return open(os.path.join(HERE, fname)).read()
-
 
 setup(
     packages=["KDEpy"],
     cmdclass={"build_ext": build_ext},
     include_dirs=[np.get_include()],
-    ext_modules=[Extension("cutils", ["KDEpy/cutils.pyx"])],
+    ext_modules=[Extension("KDEpy._cutils", ["KDEpy/cutils_ext/cutils.pyx"])],
 )
