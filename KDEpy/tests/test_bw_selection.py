13f73805b3b2787cd4152a20a7a083e5df477a85
bean217 <benpiro1118@gmail.com>
2024-03-14 16:34:15 +0100

    updated silvermans_rule to work with multidimensional data

--- KDEpy/tests/test_bw_selection.py
+++ KDEpy/tests/test_bw_selection.py
@@ -7,12 +7,16 @@ Tests for the bandwidth selection.
 import numpy as np
 import pytest
 
-from KDEpy.bw_selection import _bw_methods, improved_sheather_jones
+from KDEpy.bw_selection import _bw_methods, improved_sheather_jones, silvermans_rule
 
 
 @pytest.fixture(scope="module")
 def data() -> np.ndarray:
     return np.random.randn(100, 1)
+
+@pytest.fixture(scope="module")
+def multidim_data() -> np.ndarray:
+    return np.random.randn(100, 2)
 
 
 @pytest.mark.parametrize("method", _bw_methods.values())
@@ -20,6 +24,13 @@ def test_equal_weights_dont_changed_bw(data, method):
     weights = np.ones_like(data).squeeze() * 2
     bw_no_weights = method(data, weights=None)
     bw_weighted = method(data, weights=weights)
+    np.testing.assert_almost_equal(bw_no_weights, bw_weighted)
+
+
+def test_multidim_silvermans_rule_weights_dont_changed_bw(multidim_data):
+    weights = np.ones_like(multidim_data).squeeze() * 2
+    bw_no_weights = silvermans_rule(multidim_data, weights=None)
+    bw_weighted = silvermans_rule(multidim_data, weights=weights)
     np.testing.assert_almost_equal(bw_no_weights, bw_weighted)
 
 
@@ -43,6 +54,19 @@ def test_isj_bw_weights_same_as_resampling(data, execution_number):
         improved_sheather_jones(data_resampled),
         improved_sheather_jones(data, sample_weights),
     )
+
+
+def test_onedim_silvermans_rule_shape(data):
+    sr_res = silvermans_rule(data)
+    # dims is a float
+    assert isinstance(sr_res, float)
+
+
+def test_multidim_silvermans_rule_shape(multidim_data):
+    sr_res = silvermans_rule(multidim_data)
+    # dims shape is 2
+    dim = sr_res.shape[0]
+    assert dim == 2
 
 
 if __name__ == "__main__":
